f76253115e1dfb9151de8aee1923effa61f2ca90
Administrator <admin@example.com>
2018-08-20 15:42:03 +0800

    queue

--- 线程/python-queue.py
+++ 线程/python-queue.py
@@ -30,10 +30,13 @@ print(q.get())
 import queue
 
 q = queue.PriorityQueue(3)
+
+#put进入一个元组,元组的第一个元素是优先级(通常是数字,也可以是非数字之间的比较),数字越小优先级越高
 q.put((20,'12'))
 q.put((10,'1'))
 q.put((30,'123'))
 
+#结果(数字越小优先级越高,优先级高的优先出队)
 print(q.get())
 print(q.get())
 print(q.get())
