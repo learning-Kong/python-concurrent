29a5564a33b1051073faed91d11d31a4efb948da
Administrator <admin@example.com>
2018-08-21 15:11:26 +0800

    greenlet

--- 协程/python-greenlet.py
+++ 协程/python-greenlet.py
@@ -3,14 +3,19 @@
 
 
 from greenlet import greenlet
+import time
 
 # eta
 def eat(name):
     print('%s eat 1' % name)
+    time.sleep(10)
+    g2.switch('kong')
     print('%s eat 2' % name)
+    g2.switch()
 
 def play(name):
     print('%s play 1' % name)
+    g1.switch()
     print('%s play 2' % name)
 
 g1 = greenlet(eat)      # 创建一个greenlet对象
